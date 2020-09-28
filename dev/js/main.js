7f1d484fa306d239c40d62b186900658a16eb8ea
grammlaman <K1j1u1j1n1b1g1>
2020-09-28 21:35:18 +0600

    page complete

--- dev/js/main.js
+++ dev/js/main.js
@@ -49,9 +49,21 @@ document.querySelector('.banner-btn').onclick = function () {
     gsap.to(window, {duration: 1, scrollTo: ".services"});
 };
 
+for (let i = 1; i < 4; i++){
+    gsap.timeline({scrollTrigger: `.services-${i}`}).from(`.services-${i}`, .5, {x:-50,opacity:0}, .5);
+}
+gsap.timeline({scrollTrigger: `.foot-ask`}).from(`.foot-ask`, .5, {x:-50,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.banner-list`}).staggerFrom(`.banner-q`, .5, {x:-50,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.head`}).staggerFrom(`.head-q`, .5, {x:-50,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.c-1`}).staggerFrom(`.c-1c`, .5, {y:-15,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.c-2`}).staggerFrom(`.c-2c`, .5, {y:-15,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.c-3`}).staggerFrom(`.c-3c`, .5, {y:-15,opacity:0}, .5);
 gsap.timeline({scrollTrigger: `.c-4`}).staggerFrom(`.c-4c`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.lab-text`}).staggerFrom(`.lab-text p`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.lab-text-big`}).staggerFrom(`.lab-text-big p`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.asks-list-1`}).staggerFrom(`.asks-list-1 .asks-item label>span`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.asks-list-2`}).staggerFrom(`.asks-list-2 .asks-item label>span`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.asks-list-3`}).staggerFrom(`.asks-list-3 .asks-item label>span`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.asks-list-4`}).staggerFrom(`.asks-list-4 .asks-item label>span`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.asks-list-5`}).staggerFrom(`.asks-list-5 .asks-item label>span`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.foot-conts`}).staggerFrom(`.foot-conts-item`, .5, {x:-50,opacity:0}, .5);
