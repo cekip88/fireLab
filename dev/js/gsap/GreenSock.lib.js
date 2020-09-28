8ea70a60c8aa276d4e2fd7250b1518151358056e
grammlaman <K1j1u1j1n1b1g1>
2020-09-28 19:50:28 +0600

    add animations

--- dev/js/gsap/GreenSock.lib.js
+++ dev/js/gsap/GreenSock.lib.js
@@ -5,6 +5,7 @@ var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap, // to protect from tre
 
 export { gsapWithCSS as gsap, gsapWithCSS as default, CSSPlugin, TweenMaxWithCSS as TweenMax };
 export { TweenLite, TimelineMax, TimelineLite, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ, wrap, wrapYoyo, distribute, random, snap, normalize, getUnit, clamp, splitColor, toArray, mapRange, pipe, unitize, interpolate, shuffle } from "./gsap-core.js";
+export * from "./ScrollToPlugin.js";
 /*export * from "./Draggable.js";
 export * from "./CSSRulePlugin.js";
 export * from "./EaselPlugin.js";
--- dev/js/main.js
+++ dev/js/main.js
@@ -1,23 +1,57 @@
-/*$('.asks-slider').slick({
+import { gsap } from './gsap/GreenSock.lib.js';
+import { ScrollToPlugin } from "./gsap/ScrollToPlugin.js";
+import { ScrollTrigger } from "./gsap/ScrollTrigger.js";
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+$('.client-slider').slick({
     dots: false,
+    arrows: true,
     infinite: true,
     speed: 300,
     slidesToShow: 5,
     slidesToScroll: 1,
-    arrows: true,
+    prevArrow: $('.client-arrow.client-arrow-left'),
+    nextArrow: $('.client-arrow.client-arrow-right'),
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1025,
             settings: {
                 slidesToShow: 4,
             }
         },
         {
-            breakpoint: 350,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 3,
             }
         }
     ]
-});*/
+});
 
+
+document.querySelector('.asks-acc').onclick = function () {
+    gsap.to(window, {duration: 1, scrollTo: ".attestat"});
+};
+
+document.querySelector('.lab-link').onclick = function () {
+    gsap.to(window, {duration: 1, scrollTo: ".foot"});
+};
+
+document.querySelector('.head-callback-tablet').onclick = function () {
+    gsap.to(window, {duration: 1, scrollTo: ".foot"});
+};
+
+document.querySelector('.head-callback').onclick = function () {
+    gsap.to(window, {duration: 1, scrollTo: ".foot"});
+};
+
+document.querySelector('.banner-btn').onclick = function () {
+    gsap.to(window, {duration: 1, scrollTo: ".services"});
+};
+
+gsap.timeline({scrollTrigger: `.banner-list`}).staggerFrom(`.banner-q`, .5, {x:-50,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.head`}).staggerFrom(`.head-q`, .5, {x:-50,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.c-1`}).staggerFrom(`.c-1c`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.c-2`}).staggerFrom(`.c-2c`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.c-3`}).staggerFrom(`.c-3c`, .5, {y:-15,opacity:0}, .5);
+gsap.timeline({scrollTrigger: `.c-4`}).staggerFrom(`.c-4c`, .5, {y:-15,opacity:0}, .5);
